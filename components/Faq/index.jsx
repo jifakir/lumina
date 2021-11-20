38068105700315eb324fb5f923b24c078046b719
jifakir <jifakir@outlook.com>
2021-11-20 18:34:59 +0600

    Updated

--- components/Faq/index.jsx
+++ components/Faq/index.jsx
@@ -14,7 +14,6 @@ const Faq = () => {
                     <Accordion />
                     <Accordion />
                     <Accordion />
-                    <Accordion />
                 </div>
             </div>
         </div>
