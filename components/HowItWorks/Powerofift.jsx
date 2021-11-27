6809a28f435ea930528d7cb57369f5f4552d8580
jifakir <jifakir@outlook.com>
2021-11-27 18:55:08 +0600

    Iocn Fan Section

--- components/HowItWorks/Powerofift.jsx
+++ components/HowItWorks/Powerofift.jsx
@@ -1,19 +1,33 @@
-
-
-
 import * as React from 'react';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/dist/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 
 
 const PowerofIft = () => {
+
+    const firstRef = React.useRef();
+    const secondRef = React.useRef();
+    const thirdRef = React.useRef();
+
+    React.useEffect(() => {
+
+        gsap.timeline({duration: 1, scrollTrigger: {trigger: firstRef.current}})
+        .from(firstRef.current, {y: -10, opacity: 0})
+        .from(secondRef.current, {opacity: 0}, "<")
+        .from(thirdRef.current, {y: 10, opacity: 0}, "<")
+
+    },[])
+
     return (
         <div className="mt-16 text-xl">
-            <p className="pb-8">
+            <p ref={firstRef} className="pb-8">
                 LUMINA enables popular sports and entertainment icons and shows to create their own unique Icon Fan Token IFT®️ that can be used for numerous functions on the LUMINA exchange and ecosystem. Both Fans and Icons will have new exciting experiences in the LUM social app where it all happens.
             </p>
-            <p className="pb-8">
+            <p ref={secondRef} className="pb-8">
                 The Icon Fan Token IFT®️is at the heart of the LUMINA ecosystem. It gives fans the power to voice their opinions and positions. It also allows them to  earn gifts and immense opportunities as they exercise the privileges of their IFTs.
             </p>
-            <p className="">
+            <p ref={thirdRef} className="">
                 Icons use these IFTs on the social app to select from their most active fans for special treats like backstage meetings, auditions, featuring in new projects, sponsorships, merchandises, NFTs, autographs and many many more benefits.
             </p>
         </div>
--- components/HowItWorks/Usecases.jsx
+++ components/HowItWorks/Usecases.jsx
@@ -1,36 +1,59 @@
-
-
-
 import * as React from 'react';
-
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/dist/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 
 const UseCases = () => {
+
+    const firstRef = React.useRef();
+    const secondRef = React.useRef();
+    const thirdRef = React.useRef();
+    const forthRef = React.useRef();
+    const secHeadRef = React.useRef();
+    const fifthRef = React.useRef();
+    const sixthRef = React.useRef();
+    const seventhRef = React.useRef();
+
+    React.useEffect(() => {
+
+        gsap.timeline({duration: 0.1, scrollTrigger: {trigger: firstRef.current}})
+        .from(firstRef.current, {y: 15, opacity: 0})
+        .from(secondRef.current, {y: 15, opacity: 0})
+        .from(thirdRef.current, {y: 15, opacity: 0})
+        .from(forthRef.current, {y: 15, opacity: 0})
+        .from(secHeadRef.current, {y: 15, opacity: 0})
+        .from(fifthRef.current, {y: 15, opacity: 0})
+        .from(sixthRef.current, {y: 15, opacity: 0})
+        .from(seventhRef.current, {y: 15, opacity: 0})
+
+    },[])
+
     return (
         <div className="mt-16 text-xl">
             <h1 className="font-semibold pb-5">Thanks to the blockchain application,</h1>
             <ol className="list-decimal pb-8">
-                <li className="pb-3">
+                <li ref={firstRef} className="pb-3">
                     LUMIN is the currency on the decentralised LUMINA Exchange and ecosystem;
                 </li>
-                <li className="pb-3">
+                <li ref={secondRef} className="pb-3">
                 Exchange is a launchpad for the launch of all Icon Fan Tokens IFTs®️ ;
                 </li>
-                <li className="pb-3">
+                <li ref={thirdRef} className="pb-3">
                 With LUMIN fans acquire their IFTs. With IFTs, they interact robustly with their icons in-app in ways they could not have done on any other platform; 
                 </li>
-                <li className="">
+                <li ref={forthRef} className="">
                 LUMIN staked on the LUMINA Exchange generates rewards and IFTs for further use and interactions. 
                 </li>
             </ol>
-            <h1 className="font-semibold pb-3">IFT (Icon Fan Tokens) is used majorly for</h1>
+            <h1 ref={secHeadRef} className="font-semibold pb-3">IFT (Icon Fan Tokens) is used majorly for</h1>
             <ol className="list-decimal">
-                <li className="pb-3">
+                <li ref={fifthRef} className="pb-3">
                     Governance: fans vote through smart-contract on several binding polls published by their Icons;
                 </li>
-                <li className="pb-3">
+                <li ref={sixthRef} className="pb-3">
                 Rewards: Fans earn rewards from engagements on LUM using their holdings of IFTs.
                 </li>
-                <li className="">
+                <li ref={seventhRef} className="">
                 Staking: IFTs can be staked to earn more LUMIN tokens, Non-Fungible Token (NFT) and IFT rewards.
                 </li>
             </ol>
